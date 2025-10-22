743ec3d8f7993adc419097718736bb51aacec064
Vahid Akhtar <akhtarvahid543@gmail.com>
2025-10-22 13:31:34 +0530

    feat: domain url fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/va-portfolio/', // Replace with your actual repository name
+  base: '/', // Replace with your actual repository name
 })
